19675f4af4e3c621e1c651ab239c31544bd22aea
leahzhang1 <lzhang1@macalester.edu>
2025-09-17 09:51:43 -0500

    .

--- src/ica/03-turtle/polygon.py
+++ src/ica/03-turtle/polygon.py
@@ -1,10 +1,11 @@
+input(n)
 import turtle
 window = turtle.Screen()
 ian = turtle.Turtle()
 window.bgcolor("pink")
 ian.color("black")
 ian.begin_fill()
-for i in range(2):
+for i in range(n-1):
     ian.forward(100)
     ian.left(120)
 ian.forward(100)
